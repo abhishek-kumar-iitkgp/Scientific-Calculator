0d7a91b84d7667b66de9d3b3a5dffe85f051258e
abhishek-kumar-iitkgp <72243219+abhishek-kumar-iitkgp@users.noreply.github.com>
2021-12-25 19:52:29 +0530

    Add files via upload

---
+++ QcalcApplication.java
@@ -0,0 +1,16 @@
+package com.crio.qcalc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class QcalcApplication {
+    public static void main(String[] args) {
+        System.out.println("Starting QCalc..");
+        StandardCalculator calc = new StandardCalculator();
+
+         calc.add(1, 2);
+
+         
+        calc.printResult();
+
+    }
+}
